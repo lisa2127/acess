b254e2e8b1b769562b0754c32839d86f213e8e02
lisa2127 <69393162+lisa2127@users.noreply.github.com>
2020-08-08 20:55:14 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+/*first slider*/
+$('.slider-one')
+.not(".slick-intialized")
+.slick({
+    autoplay:true,
+    autoplayspeed:3000,
+    dots:true,
+    prevArrow:".site-slider .slider-btn .prev",
+    nextArrow:".site-slider .slider-btn .next",
+
+});
+
+/*second slider*/
+$('.slider-two')
+.not(".slick-intialized")
+.slick({
+    prevArrow:".site-slider-two .prev",
+    nextArrow:".site-slider-two .next",
+    slidesToShow:3,
+    slidesToScroll:1,
+    autoplaySpeed:3000,
+
+});
